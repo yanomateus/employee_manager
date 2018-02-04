93ba30bf7926205782bb9c14fc1e6175e86d9482
yano mateus <yano.mateus@gmail.com>
2018-02-04 20:36:25 -0200

    added employee model, the corresponding migration, and updated the settings file with employess_api app

--- website/employees_api/models.py
+++ website/employees_api/models.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    department = models.CharField(max_length=100)
--- website/website/settings.py
+++ website/website/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'employees_api.apps.EmployeesApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

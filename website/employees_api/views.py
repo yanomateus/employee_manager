2812ce4d7d09b903a531e5f0aa3ba6bbeca0e2a7
yano mateus <yano.mateus@gmail.com>
2018-02-05 12:36:13 -0200

    implemented rest endpoint to create an employee

--- website/employees_api/views.py
+++ website/employees_api/views.py
@@ -17,6 +17,8 @@ EMPLOYEE_NOT_FOUND_JSON = {'error': 'employee not found'}
 EMPLOYEE_DELETE_SUCCESS_JSON = {'msg': 'deleted'}
 EMPLOYEE_UPDATE_SUCEESS_JSON = {'msg': 'updated'}
 EMPLOYEE_EMAIL_NOT_AVAILABLE_JSON = {'error': 'email addess not available'}
+EMPLOYEE_CREATED_JSON = {'msg': 'created'}
+MISSING_DATA_JSON = {'error': 'missing data'}
 
 
 @csrf_exempt
@@ -131,4 +133,47 @@ def update_employee(request, employee_email):
 @csrf_exempt
 @require_http_methods(['POST'])
 def create_employee(request):
-    return JsonResponse({'foo': 'bar'})
+    """Implement REST endpoint to create an employee.
+
+    Example
+    -------
+    Supposing the app is running on localhost:8000, a request as::
+
+        curl -X POST -H "Content-Type: application/json" \
+            -d '{"name": "emp_name", "department": "dep_name", \
+            "email": "emp_name@mail.com"}' \
+            http://localhost:8000/employee/create/
+
+    should return a json response like::
+
+        {'msg': 'created'}
+
+    If required data is missing on the request's body then this endpoint will
+    return a json reponse like::
+
+        {'error': 'missing data'}
+
+    If the email param passed is not available then this endpoint will return
+    a json response like::
+
+        {'error': 'email addess not available'}
+
+    """
+    employee_data = json.loads(request.body)
+    try:
+        assert ('name' in employee_data and
+                'email' in employee_data and
+                'department' in employee_data)
+    except AssertionError:
+        return JsonResponse(MISSING_DATA_JSON)
+
+    new_employee = Employee()
+    new_employee.name = employee_data['name']
+    new_employee.email = employee_data['email']
+    new_employee.department = employee_data['department']
+    try:
+        new_employee.save()
+    except IntegrityError:
+        return JsonResponse(EMPLOYEE_EMAIL_NOT_AVAILABLE_JSON)
+    else:
+        return JsonResponse(EMPLOYEE_CREATED_JSON)

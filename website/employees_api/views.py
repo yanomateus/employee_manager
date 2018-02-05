3a6a55de7222066a911b4e7fe58939fbb5655141
yano mateus <yano.mateus@gmail.com>
2018-02-05 09:49:24 -0200

    implemented endpoint to delete employee

--- website/employees_api/views.py
+++ website/employees_api/views.py
@@ -6,17 +6,23 @@ from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from django.forms.models import model_to_dict
 
+from django.core.exceptions import ObjectDoesNotExist
+
 from .models import Employee
+
+
+EMPLOYEE_NOT_FOUND_JSON = {'error': 'employee not found'}
+EMPLOYEE_DELETE_SUCCESS_JSON = {'msg': 'deleted'}
 
 
 @csrf_exempt
 @require_http_methods(['GET'])
 def list_employees(request):
-    """Models the REST endpoint to list employees.
+    """Implement the REST endpoint to list employees.
 
     Example
     -------
-    Supposing the app is running on localhost, a GET request as
+    Supposing the app is running on localhost:8000, a GET request as
         $ curl -X GET -H "application/json"  https://localhost:8000/employee/
     should return a json response like
         $ [
@@ -34,9 +40,29 @@ def list_employees(request):
                         json_dumps_params={'ensure_ascii': False})
 
 
+@csrf_exempt
 @require_http_methods(['DELETE'])
 def delete_employee(request, employee_email):
-    return HttpResponse('Delete employee %s' % employee_email)
+    """Implement the REST enpoint to delete an employee.
+
+    Example
+    -------
+    Supposing the app is running on localhost:8000, a DELETE request as
+        $ curl -X DELETE -H "application/json" https://localhost:8000/employee \
+        $   /foo@bar.com/delete/
+    should return a json response like
+        $ {'msg': 'deleted'}
+    in case the user with email addres "foo@bar.com" exists. Otherwise it will
+    a json response like
+        $ {'error': 'employee not found'}
+    """
+    try:
+        employee = Employee.objects.get(email=employee_email)
+    except ObjectDoesNotExist:
+        return JsonResponse(EMPLOYEE_NOT_FOUND_JSON)
+    else:
+        employee.delete()
+        return JsonResponse(EMPLOYEE_DELETE_SUCCESS_JSON)
 
 
 def update_employee(request, employee_email):

f07d15c5d4933a2726fa1aec8c013cc4d9f0c561
yano mateus <yano.mateus@gmail.com>
2018-02-05 08:45:57 -0200

    implemented endpoint to list employees

--- website/employees_api/models.py
+++ website/employees_api/models.py
@@ -6,5 +6,8 @@ from django.db import models
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
     department = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
--- website/employees_api/views.py
+++ website/employees_api/views.py
@@ -1,12 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse  # TODO:replace by JsonResonse
+from django.http import JsonResponse
+from django.http import HttpResponse  # TODO:REMOVE ME
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
+
+from .models import Employee
 
 
+@csrf_exempt
+@require_http_methods(['GET'])
 def list_employees(request):
-    return HttpResponse('return list of employees')
+    """Models the REST endpoint to list employees.
+
+    Example
+    -------
+    Supposing the app is running on localhost, a GET request as
+        $ curl -X GET -H "application/json"  https://localhost:8000/employee/
+    should return a json response like
+        $ [
+        $   {
+        $     "name": "Ivisívis",
+        $     "email": "ivis@sivis.com",
+        $     "department": "Transparency"
+        $   }
+        $ ]
+    """
+    employees_list = [model_to_dict(emp, exclude=['id'])
+                      for emp in Employee.objects.all()]
+    return JsonResponse(employees_list,
+                        safe=False,
+                        json_dumps_params={'ensure_ascii': False})
 
 
+@require_http_methods(['DELETE'])
 def delete_employee(request, employee_email):
     return HttpResponse('Delete employee %s' % employee_email)
 
